2fa76ee42d36daf92dd7c5de55753ecf0623f5f9
Olivier Sobrie <olivier@sobrie.be>
2011-12-20 22:25:22 +0100

    [TOOLS] Add part of the differential evolution algorithm

--- tools/differential_evolution.py
+++ tools/differential_evolution.py
@@ -2,13 +2,42 @@ import sys
 sys.path.insert(0, "..")
 import random
 from mcda.types import criterion_value, criteria_values
+from mcda.types import alternatives, alternative
+from mcda.types import alternative_performances, performance_table
 from tools.generate_random import generate_random_criteria_values
+from tools.utils import get_best_alternative_performances
+from tools.utils import get_worst_alternative_performances
+
+def mutations_weights(g_weights, best_weights, r_weights, s_weights):
+    pass
 
 def mutation(model, best, r, s):
     pass
 
-def crossover(model):
-    pass
+def mutations(models, best, r, s):
+    ba = get_best_alternative_performances(best.pt, best.criteria)
+    wa = get_worst_alternative_performances(best.pt, best.criteria)
+
+    new_models = []
+    for m in models:
+        nm = mutation(model, best, r, s)
+        new_models.append(nm)
+    return new_models
+
+def crossover(models, cr):
+    n = len(models)
+    l = random.randint(0, n-1)
+
+    rdom = [ random.random() for i in range(l, n-1) ]
+    rdom.sort()
+
+    j = 0
+    for i, val in enumerate(rdom):
+        if i > 0 and val > cr:
+            j = i-1
+            break
+
+    return (l, j)
 
 def selection():
     pass
@@ -27,10 +56,32 @@ def compute_auc_k(model, k):
     score than an alternative from a worse group """
     pass
 
-def fitness(model):
-    pass
+def fitness(model, aa):
+    return compute_ca(model, aa)
 
-def init_one(c):
+def compute_models_fitness(models, aa):
+    models_fitness = {}
+    for m in models:
+        p = m.pessimist()
+        f = fitness(p, aa)
+        models_fitness[m] = f
+
+    return models_fitness
+
+def get_best_model(models_fitness):
+    best = None
+    for m, f in models_fitness.iteritems():
+        if best is None or f > best:
+            best = f
+            model = m
+            print(f)
+
+    return model
+
+def get_random_models(models):
+    return random.sample(models, 2)
+
+def init_one(c, pt, nprofiles):
     """ Generate a random ELECTRE TRI model"""
     
     # Initialize lambda value
@@ -56,39 +107,75 @@ def init_one(c):
         cval.value = random_vals[i+1]-random_vals[i]
 
     # Initialize profiles
+    b = alternatives()
+    bpt = performance_table()
+    for i in range(nprofiles):
+        id = "b%d" % (i+1)
+        b.append(alternative(id))
+        bp = alternative_performances(id, {})
+        bpt.append(bp)
 
-def initialization(n, c):
+    worst = get_worst_alternative_performances(pt, c)
+    best = get_best_alternative_performances(pt, c)
+    for crit in c:
+        worst_perf = worst(crit.id)
+        best_perf = best(crit.id)
+        rval = [ random.uniform(worst_perf, best_perf) \
+                    for i in range(nprofiles) ] 
+        rval.sort()
+        for i in range(nprofiles):
+            id = "b%d" % (i+1)
+            bp = bpt(id)
+            bp.performances[crit.id] = rval[i]
+
+    random_model = electre_tri(c, cvals, pt, bpt, lbda)
+
+    return random_model
+
+def initialization(n, c, pt, cats):
     """ Return several ELECTRE TRI models """
+    nprofiles = len(cats)-1
+    models = []
     for i in range(n):
-        init_one(c)
+        model = init_one(c, pt, nprofiles)
+        models.append(model)
+    return models
 
-def differential_evolution(c, a, aa):
+def differential_evolution(sample, mc, cr, c, a, aa, pt, cats):
     """ Learn an ELECTRE TRI model """
-    initialization(1, c)
+    models = initialization(sample, c, pt, cats)
+    models_fitness = compute_models_fitness(models, aa)
+
+    best_model = get_best_model(models_fitness)
+    print(best_model.pessimist())
+    [ h_model, s_model ] = get_random_models(models)
+    print(h_model, s_model)
+
+    crossover(models, cr)
+    mutations(models, best_model, h_model, s_model)
 
 if __name__ == "__main__":
     from tools.generate_random import generate_random_alternatives
     from tools.generate_random import generate_random_criteria
     from tools.generate_random import generate_random_criteria_values
     from tools.generate_random import generate_random_performance_table
+    from tools.generate_random import generate_random_categories
     from mcda.electre_tri import electre_tri
 
     # Create an original arbitrary model
-    a = generate_random_alternatives(200)
+    a = generate_random_alternatives(100)
     c = generate_random_criteria(3)
     cv = generate_random_criteria_values(c, 4567)
     pt = generate_random_performance_table(a, c, 1234)
 
-    b = generate_random_alternatives(1)
+    b = generate_random_alternatives(2)
     bpt = generate_random_performance_table(b, c, 0123)
+    cats = generate_random_categories(3) 
 
     lbda = 0.6
 
     model = electre_tri(c, cv, pt, bpt, lbda)
     af = model.pessimist()
+    print(af)
 
-    # Pick 20 alternatives
-    a_dm = a[:20]
-    af_dm = af[:20]
-
-    differential_evolution(c, a_dm, af_dm)
+    differential_evolution(100, 0.6, 0.6, c, a, af, pt, cats)
