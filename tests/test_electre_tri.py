0a5e5eb4487b7bb7b7e999283285c62e2044807d
Olivier Sobrie <olivier@sobrie.be>
2012-01-06 15:38:48 +0100

    [TESTS] Update electre_tri test

--- tests/test_electre_tri.py
+++ tests/test_electre_tri.py
@@ -3,69 +3,50 @@ sys.path.insert(0, "..")
 from mcda.electre_tri import electre_tri
 import unittest
 
+def compare_affectations(affectations, expected_affectations):
+        ok = 1
+        for aa in expected_affectations:
+            key = aa.alternative_id
+            if expected_affectations(key) <> affectations(key):
+                print("Pessimits affectation of %s mismatch (%d <> %d)" %
+                      (str(key), affectations(key),
+                       expected_affectations(key)))
+                ok=0
+
+        return ok
+
 class electre_tri_tests(unittest.TestCase):
 
     def test001_test_pessimist_loulouka(self):
         """ Loulouka - Pessimist """
-
         from data_loulouka import c, cv, ptb, lbda, pt, aap
-        etri = electre_tri(c, cv, ptb, lbda)
-        affectations = etri.pessimist(pt)
-        ok = 1
-        for aa in aap:
-            key = aa.alternative_id
-            if aap(key) <> affectations(key):
-                print("Pessimits affectation of %s mismatch (%d <> %d)" %
-                      (str(key), aap(key), affectations(key)))
-                ok=0
+        etri = electre_tri(c, cv, ptb, lbda).pessimist(pt)
+        ok = compare_affectations(etri, aap)
         self.assertEqual(ok, 1, "One or more affectations were wrongly \
                          assigned")
 
     def test002_test_pessimist_ticino(self):
         """ Ticino - Pessimist """
-
         from data_ticino import c, cv, ptb, lbda, pt, aap
-        etri = electre_tri(c, cv, ptb, lbda)
-        affectations = etri.pessimist(pt)
-        ok = 1
-        for aa in aap:
-            key = aa.alternative_id
-            if aap(key) <> affectations(key):
-                print("Pessimits affectation of %s mismatch (%d <> %d)" %
-                      (str(key), aap(key), affectations(key)))
-                ok=0
+        etri = electre_tri(c, cv, ptb, lbda).pessimist(pt)
+        ok = compare_affectations(etri, aap)
         self.assertEqual(ok, 1, "One or more affectations were wrongly \
                          assigned")
 
+
     def test003_test_optimist_loulouka(self):
         """ Loulouka - Optimist """
-
         from data_loulouka import c, cv, ptb, lbda, pt, aao
-        etri = electre_tri(c, cv, ptb, lbda)
-        affectations = etri.optimist(pt)
-        ok = 1
-        for aa in aao:
-            key = aa.alternative_id
-            if aao(key) <> affectations(key):
-                print("Pessimits affectation of %s mismatch (%d <> %d)" %
-                      (str(key), aao(key), affectations(key)))
-                ok=0
+        etri = electre_tri(c, cv, ptb, lbda).optimist(pt)
+        ok = compare_affectations(etri, aao)
         self.assertEqual(ok, 1, "One or more affectations were wrongly \
                          assigned")
 
     def test004_test_optimist_ticino(self):
         """ Ticino - Optimist """
-
         from data_ticino import c, cv, ptb, lbda, pt, aao
-        etri = electre_tri(c, cv, ptb, lbda)
-        affectations = etri.optimist(pt)
-        ok = 1
-        for aa in aao:
-            key = aa.alternative_id
-            if aao(key) <> affectations(key):
-                print("Pessimits affectation of %s mismatch (%d <> %d)" %
-                      (str(key), aao(key), affectations(key)))
-                ok=0
+        etri = electre_tri(c, cv, ptb, lbda).optimist(pt)
+        ok = compare_affectations(etri, aao)
         self.assertEqual(ok, 1, "One or more affectations were wrongly \
                          assigned")
 
