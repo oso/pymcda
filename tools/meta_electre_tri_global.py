2beb796fb836abf6951ab849964bcfbd868ab27b
Olivier Sobrie <olivier@sobrie.be>
2012-02-08 22:49:39 +0100

    [META_ETRI_GLOBAL] First steps of the new algorithm

--- tools/meta_electre_tri_global.py
+++ tools/meta_electre_tri_global.py
@@ -2,6 +2,8 @@ from __future__ import division
 import sys
 sys.path.insert(0, "..")
 import random
+from itertools import product
+
 from mcda.electre_tri import electre_tri
 from mcda.types import alternative_affectation, alternatives_affectations
 from tools.lp_electre_tri_weights import lp_elecre_tri_weights
@@ -30,7 +32,95 @@ class meta_electre_tri_global():
         self.bp = get_best_alternative_performances(pt, c)
         self.n_intervals = 3
 
-    def compute_fitness(self, model, aa):
+    def get_interval(self, perf, intervals):
+        i = 0;
+        while i < len(intervals) and perf > intervals[i]:
+            i += 1
+        return i
+
+    def update_one_profile(self, model, aa, p_id):
+        current = model.profiles[p_id].performances
+
+        if p_id == 0:
+            below = self.b0.performances
+        else:
+            below = model.profiles[p_id-1].performances
+
+        if p_id == len(model.profiles)-1:
+            above = self.bp.performances
+        else:
+            above = model.profiles[p_id+1].performances
+
+        above_intervals = {}
+        below_intervals = {}
+        for c in model.criteria:
+            below_interval = (current[c.id]-below[c.id])/self.n_intervals
+            above_interval = (above[c.id]-current[c.id])/self.n_intervals
+
+            below_intervals[c.id] = [ below[c.id] + i*below_interval
+                                      for i in range(1, self.n_intervals) ]
+            above_intervals[c.id] = [ current[c.id] + i*above_interval
+                                      for i in range(1, self.n_intervals) ]
+
+        histograms = { c.id: [ 0 for i in range(2*self.n_intervals)]
+                       for c in model.criteria }
+        for ap, c in product(self.pt, model.criteria):
+            aperf = ap.performances[c.id]
+            pperf = current[c.id]
+            histogram = histograms[c.id]
+
+            if aperf > above[c.id]:
+                print 'above', aperf, above[c.id]
+                continue
+            if aperf < below[c.id]:
+                print 'below', aperf, below[c.id]
+                continue
+
+            af = aa(ap.alternative_id)
+            af_ori = self.aa(ap.alternative_id)
+
+            rank = self.categories(af).rank
+            rank_ori = self.categories(af_ori).rank
+
+            if aperf > pperf:
+                 i = self.get_interval(aperf, above_intervals[c.id])
+                 i = i + self.n_intervals
+                 if rank > rank_ori:    #Profile too low
+                    histogram[i] += 1
+                 elif rank == rank_ori:
+                    histogram[i] -= 1
+
+            elif aperf < pperf:
+                 i = self.get_interval(aperf, below_intervals[c.id])
+                 if rank < rank_ori:    #Profile too high
+                    histogram[i] += 1
+                 elif rank == rank_ori:
+                    histogram[i] -= 1
+
+#        print histograms
+        for c_id, h in histograms.iteritems():
+            m = max(h)
+            if m > 0:
+                i = h.index(m)
+                print "*", m, i
+                if i == 0:
+                    current[c_id] = below[c_id]
+                elif i == 2*self.n_intervals-1:
+                    current[c_id] = above[c_id]
+                elif i < self.n_intervals:
+                    current[c_id] = below_intervals[c_id][i-1]
+                else:
+                    i -= self.n_intervals
+                    current[c_id] = above_intervals[c_id][i-1]
+
+#            print ap.alternative_id, c.id
+
+#        interval = (current-below)/self.n_intervals
+#        below_intervals = [ below + i*interval
+#                            for i in range(1, self.n_intervals) ]
+#        below_intervals.insert(0, 
+
+    def update_profiles(self, model, aa):
         c_perfs = {c.id:list() for c in model.criteria}
         for i, p in enumerate(model.profiles):
             perfs = p.performances
@@ -40,19 +130,22 @@ class meta_electre_tri_global():
             c_perfs[c.id].insert(0, self.b0.performances[c.id])
             c_perfs[c.id].append(self.bp.performances[c.id])
 
-        # Constructs the intervals
-        for c in model.criteria:
-            perfs = c_perfs[c.id]
-            for h in range(1, len(model.profiles)+1):
-                perf_profile = perfs[h];
-                perf_profile_inf = perfs[h-1]
-                perf_profile_sup = perfs[h+1]
-                print perf_profile
-                print perf_profile_inf
-                print perf_profile_sup
+        for i in range(len(model.profiles)):
+            self.update_one_profile(model, aa, i);
+
+    def compute_fitness(self, model, aa):
+#        # Constructs the intervals
+#        for c in model.criteria:
+#            perfs = c_perfs[c.id]
+#            for h in range(1, len(model.profiles)+1):
+#                perf_profile = perfs[h];
+#                perf_profile_inf = perfs[h-1]
+#                perf_profile_sup = perfs[h+1]
+#                print perf_profile
+#                print perf_profile_inf
+#                print perf_profile_sup
 #                c_perfs_interval_min[h][c.id] = []
 #                c_perfs_interval_plus[h][c.id] = []
-        raise
 
         total = len(self.alternatives)
         nok = 0
@@ -107,6 +200,8 @@ class meta_electre_tri_global():
             fitness = self.compute_fitness(model, aa)
             models_fitness[model] = fitness
 
+            self.update_profiles(model, aa)
+
         return models_fitness
 
     # Input params:
@@ -136,15 +231,15 @@ if __name__ == "__main__":
     from mcda.electre_tri import electre_tri
 
     # Original Electre Tri model
-    a = generate_random_alternatives(10)
+    a = generate_random_alternatives(100)
     c = generate_random_criteria(5)
     cv = generate_random_criteria_values(c, 4567)
     normalize_criteria_weights(cv)
     pt = generate_random_performance_table(a, c, 1234)
 
-    b = generate_random_alternatives(2, 'b')
+    b = generate_random_alternatives(1, 'b')
     bpt = generate_random_categories_profiles(b, c, 2345)
-    cat = generate_random_categories(3)
+    cat = generate_random_categories(2)
 
     lbda = 0.75
 
@@ -162,7 +257,7 @@ if __name__ == "__main__":
     meta_global = meta_electre_tri_global(a, c, cv, aa, pt, cat)
 
     t1 = time.time()
-    m = meta_global.solve(100, 10, 5)
+    m = meta_global.solve(10, 100, 5)
     t2 = time.time()
     print("Computation time: %g secs" % (t2-t1))
 
