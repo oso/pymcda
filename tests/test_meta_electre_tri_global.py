7842ae969471fc9963286868ddfe429105ede753
Olivier Sobrie <olivier@sobrie.be>
2012-02-20 23:15:12 +0100

    [TESTS] Update test of the heuristic used to compute new profiles

--- tests/test_meta_electre_tri_global.py
+++ tests/test_meta_electre_tri_global.py
@@ -10,6 +10,7 @@ from mcda.electre_tri import electre_tri
 from tools.meta_electre_tri_global import heuristic_profiles
 from tools.utils import get_best_alternative_performances
 from tools.utils import get_worst_alternative_performances
+from tools.utils import get_pc_of_wrong_assignment
 from tools.generate_random import generate_random_alternatives
 from tools.generate_random import generate_random_criteria
 from tools.generate_random import generate_random_criteria_values
@@ -19,16 +20,19 @@ from tools.generate_random import generate_random_categories_profiles
 from tools.utils import normalize_criteria_weights
 
 seeds = [ 123, 456, 789, 12, 345, 678, 901, 234, 567, 890 ]
+#seeds = [ 901 ]
 
 class heuristic_profiles_tests(unittest.TestCase):
 
     def variable_number_alternatives_and_criteria(self, ncat):
-        n_alts = [ i*100 for i in range(1, 11) ]
-        n_alts.extend([ i*1000 for i in range(1, 11) ])
-        n_crit = [ i for i in range(2,21) ]
+#        n_alts = [ i*100 for i in range(1, 11) ]
+#        n_alts.extend([ i*1000 for i in range(1, 11) ])
+#        n_crit = [ i for i in range(2,21) ]
+        n_alts = [ 100 ]
+        n_crit = [ 1 ]
 
-        objectives = { nc: {na: dict() for na in n_alts} for nc in n_crit }
-        times = { nc: {na: dict() for na in n_alts} for nc in n_crit }
+        print ''
+        nloop = { nc: {na: dict() for na in n_alts} for nc in n_crit }
         errors = { nc: {na: dict() for na in n_alts} for nc in n_crit }
         for nc, na, seed in product(n_crit, n_alts, seeds):
             a = generate_random_alternatives(na)
@@ -46,13 +50,23 @@ class heuristic_profiles_tests(unittest.TestCase):
             model = electre_tri(c, cv, bpt, lbda, cat)
             aa = model.pessimist(pt)
 
+            model.profiles.display()
+
             b0 = get_worst_alternative_performances(pt, c)
             bp = get_best_alternative_performances(pt, c)
-            heur = heuristic_profiles(model, a, c, pt, aa, b0, bp)
 
-            for j in range(20):
-                heur.optimize()
-            aa_learned = model.pessimist(pt)
+            for j in range(1):
+                model.profiles = generate_random_categories_profiles(b, c)
+                heur = heuristic_profiles(model, a, c, pt, aa, b0, bp)
+                for k in range(100):
+                    aa2 = model.pessimist(pt)
+                    wrong = get_pc_of_wrong_assignment(aa, aa2)
+                    if wrong == 0:
+                        break;
+                    heur.optimize(aa2)
+                    model.profiles.display(header=None)
+
+                aa_learned = model.pessimist(pt)
 
             total = len(a)
             nok = 0
@@ -60,29 +74,28 @@ class heuristic_profiles_tests(unittest.TestCase):
                 if aa(alt.id) != aa_learned(alt.id):
                     nok += 1
 
+            nloop[nc][na][seed] = k
             errors[nc][na][seed] = nok/total
 
-            print("%d\t%d\t%s\t%-6.5f" % (nc, na, seed, nok/total))
+            print("%d\t%d\t%s\t%d\t%-6.5f" % (nc, na, seed, k, nok/total))
 
         print('Summary')
         print('========')
         print("nseeds: %d" % len(seeds))
-        print('nc\tna\tobj\ttime\terrors')
+        print('nc\tna\tnloop\terrors')
         for nc, na in product(n_crit, n_alts):
-            obj = sum(objectives[nc][na].values())/len(seeds)
-            tim = sum(times[nc][na].values())/len(seeds)
+            n = sum(nloop[nc][na].values())/len(seeds)
             err = sum(errors[nc][na].values())/len(seeds)
-            print("%d\t%d\t%-6.5f\t%-6.5f\t%-6.5f" % (nc, na, obj, tim,
-                  err))
+            print("%d\t%d\t%d\t%-6.5f" % (nc, na, n, err))
 
     def test001_two_categories(self):
-        self.variable_number_alternatives_and_criteria(2)
-
-    def test002_three_categories(self):
         self.variable_number_alternatives_and_criteria(3)
 
-    def test003_four_categories(self):
-        self.variable_number_alternatives_and_criteria(4)
+#    def test002_three_categories(self):
+#        self.variable_number_alternatives_and_criteria(3)
+#
+#    def test003_four_categories(self):
+#        self.variable_number_alternatives_and_criteria(4)
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
