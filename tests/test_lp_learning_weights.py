c5dff2055e9198d350909d224ec1da11e37445d2
Olivier Sobrie <olivier@sobrie.be>
2012-06-29 15:09:59 +0200

    [TESTS] lp_weights: separate learning from generalization

--- tests/test_lp_learning_weights.py
+++ tests/test_lp_learning_weights.py
@@ -16,18 +16,18 @@ from tools.generate_random import generate_random_performance_table
 from tools.generate_random import generate_random_categories
 from tools.generate_random import generate_random_profiles
 from tools.generate_random import generate_random_categories_profiles
+from tools.utils import compute_ac
 from tools.utils import normalize_criteria_weights
 from tools.utils import add_errors_in_affectations
 
-seeds = [ 123, 456, 789, 12, 345, 678, 901, 234, 567, 890 ]
+#seeds = [ 123, 456, 789, 12, 345, 678, 901, 234, 567, 890 ]
+seeds = [ 123 ]
 
-def variable_number_alternatives_and_criteria(ncat, er=0, nlearn=1.0):
-    n_alts = [ i*1000 for i in range(1, 11) ]
-    n_crit = [ i for i in range(2,21) ]
+def variable_number_alternatives_and_criteria(ncat, er = 0, na_gen = 10000):
     n_alts = [ i*1000 for i in range(1, 11) ]
     n_crit = [ 5, 7, 10]
 
-    print('\nnc\tna\tncat\tnlearn\terr\tseed\tobj\terrors\terr_bad'
+    print('\nnc\tna\tncat\tna_gen\terr\tseed\tobj\terrors\terr_bad\tca'
           '\tt_total\tt_const\tt_solve')
 
     objectives = { nc: {na: dict() for na in n_alts} for nc in n_crit }
@@ -38,7 +38,9 @@ def variable_number_alternatives_and_criteria(ncat, er=0, nlearn=1.0):
     errors_min = { nc: {na: dict() for na in n_alts} for nc in n_crit }
     errors_max = { nc: {na: dict() for na in n_alts} for nc in n_crit }
     errors_erroned = { nc: {na: dict() for na in n_alts} for nc in n_crit }
+    cas = { nc: {na: dict() for na in n_alts} for nc in n_crit }
     for nc, na, seed in product(n_crit, n_alts, seeds):
+        # Generate a random ELECTRE TRI model and assignment examples
         a = generate_random_alternatives(na)
         c = generate_random_criteria(nc)
         cv = generate_random_criteria_values(c, seed)
@@ -53,18 +55,16 @@ def variable_number_alternatives_and_criteria(ncat, er=0, nlearn=1.0):
         lbda = random.uniform(0.5, 1)
 
         model = electre_tri(c, cv, bpt, lbda, cps)
+        model2 = model.copy()
         aa = model.pessimist(pt)
 
-        a_learn = random.sample(a, int(nlearn*len(a)))
-        aa_learn = alternatives_affectations([ aa[alt.id]
-                                               for alt in a_learn ])
-        pt_learn = performance_table([ pt[alt.id] for alt in a_learn ])
-
-        aa_err = aa_learn.copy()
+        # Add errors in assignment examples
+        aa_err = aa.copy()
         aa_erroned = add_errors_in_affectations(aa_err, cat.get_ids(), er)
 
+        # Run linear program
         t1 = time.time()
-        lp_weights = lp_electre_tri_weights(model, pt_learn, aa_err, cps,
+        lp_weights = lp_electre_tri_weights(model2, pt, aa_err, cps,
                                             0.0001)
         t2 = time.time()
         obj = lp_weights.solve()
@@ -75,46 +75,53 @@ def variable_number_alternatives_and_criteria(ncat, er=0, nlearn=1.0):
         times_const[nc][na][seed] = t2-t1
         times_solve[nc][na][seed] = t3-t2
 
-        aa_learned = model.pessimist(pt)
-
-        total = len(a)
+        # Compute new assignment and classification accuracy
+        aa2 = model.pessimist(pt)
         nok = nok_erroned = 0
-        a_assign = { alt.alternative_id: alt.category_id for alt in aa }
-        a_assign2 = { alt.alternative_id: alt.category_id
-                      for alt in aa_learned}
         for alt in a:
-            if a_assign[alt.id] != a_assign2[alt.id]:
+            if aa(alt.id) != aa2(alt.id):
                 nok += 1
                 if alt.id in aa_erroned:
                     nok_erroned += 1
 
-        e = nok/total
-        errors[nc][na][seed] = nok / total
-        e_err = nok_erroned / total
+        total = len(a)
 
+        e = nok/total
+        e_err = nok_erroned / total
+        errors[nc][na][seed] = nok / total
         errors_erroned[nc][na][seed] = e_err
 
-        print("%d\t%d\t%d\t%-6.4f\t%-6.4f\t%s\t%-6.4f\t%-6.5f\t%-6.5f"
-              "\t%-6.5f\t%-6.5f\t%-6.5f" % (nc, na, ncat, nlearn, er, seed,
-              obj, e, e_err, t3-t1, t2-t1, t3-t2))
+        # Perform the generalization
+        a_gen = generate_random_alternatives(na_gen)
+        pt_gen = generate_random_performance_table(a_gen, c)
+        aa = model.pessimist(pt_gen)
+        aa2 = model2.pessimist(pt_gen)
+        ca = compute_ac(aa, aa2)
+        cas[nc][na][seed] = ca
+        print 'ca', ca
+
+        print("%d\t%d\t%d\t%d\t%-6.4f\t%s\t%-6.4f\t%-6.5f\t%-6.5f"
+              "\t%-6.5f\t%-6.5f\t%-6.5f\t%-6.5f" % (nc, na, ncat, na_gen,
+              er, seed, obj, e, e_err, ca, t3-t1, t2-t1, t3-t2))
 
     print('Summary')
     print('========')
     print("nseeds: %d" % len(seeds))
-    print('nc\tna\tncat\tnlearn\terr\tobj\terr_avg\terr_min\terr_max' \
-          '\terr_bad\tt_total\tt_cons\tt_solve')
+    print('nc\tna\tncat\tna_gen\terr\tobj\terr_avg\terr_min\terr_max' \
+          '\terr_bad\tca\tt_total\tt_cons\tt_solve')
     for nc, na in product(n_crit, n_alts):
-        obj = sum(objectives[nc][na].values())/len(seeds)
-        tim_tot = sum(times_total[nc][na].values())/len(seeds)
-        tim_con = sum(times_const[nc][na].values())/len(seeds)
-        tim_sol = sum(times_solve[nc][na].values())/len(seeds)
-        err = sum(errors[nc][na].values())/len(seeds)
+        obj = sum(objectives[nc][na].values()) / len(seeds)
+        tim_tot = sum(times_total[nc][na].values()) / len(seeds)
+        tim_con = sum(times_const[nc][na].values()) / len(seeds)
+        tim_sol = sum(times_solve[nc][na].values()) / len(seeds)
+        err = sum(errors[nc][na].values()) / len(seeds)
         err_min = min(errors[nc][na].values())
         err_max = max(errors[nc][na].values())
-        err_erroned = sum(errors_erroned[nc][na].values())/len(seeds)
-        print("%d\t%d\t%d\t%-6.5f\t%-6.5f\t%-6.4f\t%-6.5f\t%-6.5f\t%-6.5f"
-              "\t%-6.5f\t%-6.5f\t%-6.5f\t%-6.5f" % (nc, na, ncat,
-              nlearn, er, obj, err, err_min, err_max, err_erroned,
+        err_erroned = sum(errors_erroned[nc][na].values()) / len(seeds)
+        ca = sum(cas[nc][na].values()) / len(seeds)
+        print("%d\t%d\t%d\t%d\t%-6.5f\t%-6.4f\t%-6.5f\t%-6.5f\t%-6.5f"
+              "\t%-6.5f\t%-6.5f\t%-6.5f\t%-6.5f\t%-6.5f" % (nc, na, ncat,
+              na_gen, er, obj, err, err_min, err_max, err_erroned, ca,
               tim_tot, tim_con, tim_sol))
 
 class tests_lp_electre_tri_weights(unittest.TestCase):
@@ -157,66 +164,9 @@ class tests_lp_electre_tri_weights_with_errors(unittest.TestCase):
     def test008_three_categories_errors_40pc(self):
         variable_number_alternatives_and_criteria(3, 0.4)
 
-class test_lp_electre_tri_weights_subset(unittest.TestCase):
-
-    def test001_two_categories_9pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.09)
-
-    def test002_two_categories_8pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.08)
-
-    def test003_two_categories_7pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.07)
-
-    def test004_two_categories_6pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.06)
-
-    def test005_two_categories_5pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.05)
-
-    def test006_two_categories_4pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.04)
-
-    def test007_two_categories_3pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.03)
-
-    def test008_two_categories_2pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.02)
-
-    def test008_two_categories_1pc(self):
-        variable_number_alternatives_and_criteria(2, 0.0, 0.01)
-
-    def test001_three_categories_9pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.09)
-
-    def test002_three_categories_8pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.08)
-
-    def test003_three_categories_7pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.07)
-
-    def test004_three_categories_6pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.06)
-
-    def test005_three_categories_5pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.05)
-
-    def test006_three_categories_4pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.04)
-
-    def test007_three_categories_3pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.03)
-
-    def test008_three_categories_2pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.02)
-
-    def test008_three_categories_1pc(self):
-        variable_number_alternatives_and_criteria(3, 0.0, 0.01)
-
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite1 = loader.loadTestsFromTestCase(tests_lp_electre_tri_weights)
     suite2 = loader.loadTestsFromTestCase(tests_lp_electre_tri_weights_with_errors)
-    suite3 = loader.loadTestsFromTestCase(test_lp_electre_tri_weights_subset)
-    alltests = unittest.TestSuite([suite1, suite2, suite3])
+    alltests = unittest.TestSuite([suite1, suite2])
     unittest.TextTestRunner(verbosity=2).run(alltests)
