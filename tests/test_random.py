d914c11fc684c1c8999fc184cd3df780f9aa2279
Olivier Sobrie <olivier@sobrie.be>
2012-12-17 18:03:47 +0100

    test_random: add test of random uniformity

---
+++ tests/test_random.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(0, "..")
+from tools.generate_random import generate_random_criteria
+from tools.generate_random import generate_random_criteria_weights
+from tools.generate_random import generate_random_criteria_values
+from tools.utils import normalize_criteria_weights
+
+n = 10000
+
+c = generate_random_criteria(3)
+cw = generate_random_criteria_weights(c)
+
+cids = c.keys()
+
+f = open('data/random_1.txt', 'wb')
+
+for i in range(n):
+    cw = generate_random_criteria_weights(c)
+    for cid in cids:
+        f.write(str(cw[cid].value) + "\t")
+    f.write("\n")
+
+f.close()
+
+f = open('data/random_2.txt', 'wb')
+
+for i in range(n):
+    cw = generate_random_criteria_values(c)
+    normalize_criteria_weights(cw)
+    for cid in cids:
+        f.write(str(cw[cid].value) + "\t")
+    f.write("\n")
+
+f.close()
