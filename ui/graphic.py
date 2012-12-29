839c7d2e937ea780bc86cdedfb668f4c1a1f1e21
Olivier Sobrie <olivier@sobrie.be>
2012-12-29 22:28:01 +0100

    graphic: QGraphicsScene_etri add update_profiles/categories method
    Add methods to update the profile and categories in the graphic view

--- ui/graphic.py
+++ ui/graphic.py
@@ -281,10 +281,85 @@ class QGraphicsScene_etri(QtGui.QGraphicsScene):
 
             self.intersection_items.append(item)
 
+    def update_intersections(self):
+        self.__clear_intersections()
+        self.__higlight_intersections()
+
     def __plot_alternatives(self):
         for ap, item in self.ap_items.items():
             item, text_items = self.__create_profile(ap)
             self.addItem(item)
+
+    def __update_category(self, i, cat, ap_below, ap_above):
+        item = self.category_items[cat]
+        path = item.path()
+
+        for i, cid in enumerate(self.criteria_order):
+            y_above = self.__compute_y(ap_above, cid)
+            y_below = self.__compute_y(ap_below, cid)
+
+            i2 = 2 * len(self.criteria_order) - 1 - i
+            if i == 0:
+                x = 0
+            else:
+                x += self.hspacing
+
+            path.setElementPositionAt(i, x, y_above)
+            path.setElementPositionAt(i2, x, y_below)
+
+        item.setPath(path)
+
+    def update_categories(self):
+        ncat = len(self.model.categories)
+        for i, cat in enumerate(self.model.categories):
+            if i == 0:
+                ap_below = self.worst
+            else:
+                ap_below = self.model.bpt[self.model.profiles[i - 1]]
+
+            if i == ncat - 1:
+                ap_above = self.best
+            else:
+                ap_above = self.model.bpt[self.model.profiles[i]]
+
+            self.__update_category(i, cat, ap_below, ap_above)
+
+        self.update_intersections()
+
+    def update_profile(self, profile):
+        item = self.profiles_items[profile]
+        text_items = self.profiles_text_items[profile]
+        if profile == 'worst':
+            ap = self.worst
+        elif profile == 'best':
+            ap = self.best
+        else:
+            ap = self.model.bpt[profile]
+
+        path = item.path()
+
+        for i, cid in enumerate(self.criteria_order):
+            y = self.__compute_y(ap, cid)
+            if y > self.worst.performances[cid]:
+                y = self.worst.performances[cid]
+
+            if i == 0:
+                x = 0
+            else:
+                x += self.hspacing
+
+            path.setElementPositionAt(i, x, y)
+
+            if len(text_items) > 0:
+                txtitem = text_items[i]
+                txtitem.setPos(x, y)
+                txtitem.setPlainText("%g" % ap.performances[cid])
+
+        item.setPath(path)
+
+    def update_profiles(self):
+        for profile in self.profiles_items.keys():
+            self.update_profile(profile)
 
     def plot_alternative_performances(self, ap):
         item, text_items = self.__create_profile(ap)
