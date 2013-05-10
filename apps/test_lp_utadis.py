199389df28d807aed2636567a15b2e73ae0c7fd4
Olivier Sobrie <olivier@sobrie.be>
2013-05-10 12:58:24 +0200

    test_lp_utadis: code cleanup, etc

--- apps/test_lp_utadis.py
+++ apps/test_lp_utadis.py
@@ -13,43 +13,37 @@ from pymcda.types import CriterionValue, CriteriaValues
 from pymcda.uta import Utadis
 from pymcda.learning.lp_utadis import LpUtadis
 from pymcda.generate import generate_alternatives
-from pymcda.generate import generate_criteria
-from pymcda.generate import generate_random_criteria_values
 from pymcda.generate import generate_random_performance_table
-from pymcda.generate import generate_categories
-from pymcda.generate import generate_random_categories_values
-from pymcda.generate import generate_random_criteria_functions
+from pymcda.generate import generate_random_utadis_model
 from pymcda.utils import compute_ca
-from pymcda.utils import add_errors_in_assignments
+from pymcda.utils import add_errors_in_assignments_proba
 from test_utils import test_result, test_results
 
 def test_lp_utadis(seed, na, nc, ncat, ns, na_gen, pcerrors):
-    # Generate a random ELECTRE TRI model and assignment examples
-    c = generate_criteria(nc)
-    cv = generate_random_criteria_values(c, seed)
-    cv.normalize()
-    cat = generate_categories(ncat)
-
-    cfs = generate_random_criteria_functions(c, nseg_min = ns,
-                                             nseg_max = ns)
-    catv = generate_random_categories_values(cat)
+    # Generate a random UTADIS model and assignment examples
+    model = generate_random_utadis_model(nc, ncat, ns, ns)
+    model.set_equal_weights()
+    cat = model.cat_values.to_categories()
 
     a = generate_alternatives(na)
-    pt = generate_random_performance_table(a, c)
-
-    model = Utadis(c, cv, cfs, catv)
+    pt = generate_random_performance_table(a, model.criteria)
     aa = model.get_assignments(pt)
 
     # Add errors in assignment examples
     aa_err = aa.copy()
-    aa_erroned = add_errors_in_assignments(aa_err, cat.keys(),
-                                           pcerrors / 100)
+    aa_erroned = add_errors_in_assignments_proba(aa_err, cat.keys(),
+                                                 pcerrors / 100)
+    na_err = len(aa_erroned)
 
-    gi_worst = AlternativePerformances('worst', {crit.id: 0 for crit in c})
-    gi_best = AlternativePerformances('best', {crit.id: 1 for crit in c})
+    gi_worst = AlternativePerformances('worst',
+                                       {crit.id: 0
+                                        for crit in model.criteria})
+    gi_best = AlternativePerformances('best',
+                                      {crit.id: 1
+                                       for crit in model.criteria})
 
     css = CriteriaValues([])
-    for cf in cfs:
+    for cf in model.cfs:
         cs = CriterionValue(cf.id, len(cf.function))
         css.append(cs)
 
@@ -60,7 +54,7 @@ def test_lp_utadis(seed, na, nc, ncat, ns, na_gen, pcerrors):
     obj, cv_l, cfs_l, catv_l = lp.solve(aa_err, pt)
     t3 = time.time()
 
-    model2 = Utadis(c, cv_l, cfs_l, catv_l)
+    model2 = Utadis(model.criteria, cv_l, cfs_l, catv_l)
 
     # Compute new assignment and classification accuracy
     aa2 = model2.get_assignments(pt)
@@ -86,10 +80,17 @@ def test_lp_utadis(seed, na, nc, ncat, ns, na_gen, pcerrors):
 
     # Perform the generalization
     a_gen = generate_alternatives(na_gen)
-    pt_gen = generate_random_performance_table(a_gen, c)
-    aa = model.get_assignments(pt_gen)
-    aa2 = model2.get_assignments(pt_gen)
-    ca_gen = compute_ca(aa, aa2)
+    pt_gen = generate_random_performance_table(a_gen, model.criteria)
+    aa_gen = model.get_assignments(pt_gen)
+    aa_gen2 = model2.get_assignments(pt_gen)
+    ca_gen = compute_ca(aa_gen, aa_gen2)
+
+    aa_gen_err = aa_gen.copy()
+    aa_gen_erroned = add_errors_in_assignments_proba(aa_gen_err,
+                                                     cat.keys(),
+                                                     pcerrors / 100)
+    aa_gen2 = model2.get_assignments(pt_gen)
+    ca_gen_err = compute_ca(aa_gen_err, aa_gen2)
 
     # Save all infos in test_result class
     t = test_result("%s-%d-%d-%d-%d-%d-%g" % (seed, na, nc, ncat, ns,
@@ -105,12 +106,14 @@ def test_lp_utadis(seed, na, nc, ncat, ns, na_gen, pcerrors):
     t['pcerrors'] = pcerrors
 
     # Output params
+    t['na_err'] = na_err
     t['obj'] = obj
     t['ca'] = ca
     t['ca_errors'] = ca_errors
     t['ca2'] = ca2
     t['ca2_errors'] = ca2_errors
     t['ca_gen'] = ca_gen
+    t['ca_gen_err'] = ca_gen_err
     t['t_total'] = t3 - t1
     t['t_const'] = t2 - t1
     t['t_solve'] = t3 - t2
@@ -158,8 +161,9 @@ def run_tests(na, nc, ncat, ns, na_gen, pcerrors, nseeds, filename):
     # Perform a summary
     writer.writerow(['', ''])
     t = results.summary(['na', 'nc', 'ncat', 'ns', 'na_gen', 'pcerrors'],
-                        ['obj', 'ca', 'ca_errors', 'ca2', 'ca2_errors',
-                         'ca_gen', 't_total', 't_const', 't_solve'])
+                        ['na_err' 'obj', 'ca', 'ca_errors', 'ca2',
+                         'ca2_errors', 'ca_gen', 'ca_gen_err', 't_total',
+                         't_const', 't_solve'])
     t.tocsv(writer)
 
 if __name__ == "__main__":
