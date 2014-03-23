84306c915296140880a69d83a95d319b83490bff
Olivier Sobrie <olivier@sobrie.be>
2014-03-23 21:58:16 +0100

    count_veto_effects: adaptation for the new veto generation rule

--- apps/count_veto_effects.py
+++ apps/count_veto_effects.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from __future__ import print_function
 import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/../")
 import math
@@ -6,13 +7,30 @@ import random
 from itertools import product
 
 from pymcda.generate import generate_random_mrsort_model_with_coalition_veto
+from pymcda.generate import generate_random_mrsort_model_with_binary_veto
 from pymcda.generate import generate_alternatives
 from pymcda.generate import generate_random_performance_table
 
-def count_veto(seed, na, nc, ncat, veto_interval):
+BINARY_VETO = 1
+COALITIONS_VETO = 2
+
+veto_function = 2 # 1 = absolute veto; 2 = prop veto
+veto_type = COALITIONS_VETO
+veto_weights = True
+
+def count_veto(seed, na, nc, ncat, veto_param):
     # Generate a random ELECTRE TRI BM model
-    model = generate_random_mrsort_model_with_coalition_veto(nc, ncat, seed,
-                                        veto_interval = veto_interval / 100)
+    if veto_type == COALITIONS_VETO:
+        model = generate_random_mrsort_model_with_coalition_veto(nc, ncat,
+                                            seed,
+                                            veto_weights = True,
+                                            veto_func = veto_function,
+                                            veto_param = veto_param)
+    elif veto_type == BINARY_VETO:
+        model = generate_random_mrsort_model_with_binary_veto(nc, ncat,
+                                            seed,
+                                            veto_func = veto_function,
+                                            veto_param = veto_param)
 
     # Generate a set of alternatives
     a = generate_alternatives(na)
@@ -22,20 +40,33 @@ def count_veto(seed, na, nc, ncat, veto_interval):
     return sum([model.count_veto_pessimist(ap) for ap in pt])
 
 if __name__ == "__main__":
-    nas = range(10, 101, 10)
-    ncs = [ 3, 5, 7, 10 ]
+#    nas = range(10, 101, 10)
+    nas = [ 100 ]
+    ncs = [ 5 ]
     ncats = [ 2, 3, 4, 5 ]
-    veto_intervals = [ 80 ]
+    veto_params = [ .1, .2, .3, .4, .5, .6, .7, .8, .9, 1 ]
     nseeds = 10
 
-    print("na\tnc\tncat\tvinterv\tnv_avg\tnv_std\tnv_min\tnv_max")
-    for nc, ncat, na, veto_interval in product(ncs, ncats, nas, veto_intervals):
+    nv_avgs = {}
+    nv_stds = {}
+    print("na\tnc\tncat\tvparam\tnv_avg\tnv_std\tnv_min\tnv_max")
+    for nc, ncat, na, veto_param in product(ncs, ncats, nas, veto_params):
         nv = []
         for i in range(nseeds):
-            nv.append(count_veto(i, na, nc, ncat, veto_interval))
+            nv.append(count_veto(i, na, nc, ncat, veto_param))
         nv_avg = sum(nv) / nseeds
         nv_std = math.sqrt(sum((map(lambda x: (x - nv_avg)**2, nv))) / nseeds)
         nv_min, nv_max = min(nv), max(nv)
-        print("%d\t%d\t%d\t%g\t%g\t%g\t%g\t%g" % (na, nc, ncat, veto_interval,
+
+        nv_avgs[na,nc,ncat] = nv_avg
+        nv_stds[na,nc,ncat] = nv_std
+        print("%d\t%d\t%d\t%g\t%g\t%2g\t%d\t%d" % (na, nc, ncat, veto_param,
                                                   nv_avg, nv_std, nv_min,
                                                   nv_max))
+
+    for nc, na, veto_param in product(ncs, nas, veto_params):
+        print("%d & %d & %d" % (nc, na, veto_param), end = '')
+        for ncat in ncats:
+            print(" & $%.2f \pm %.2f$" % (nv_avgs[na,nc,ncat],
+                                        nv_stds[na,nc,ncat]), end = '')
+        print("\\\\")
