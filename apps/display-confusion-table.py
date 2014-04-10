dcbd65d890cb88eb51fa3b79820c3c9049b54f40
Olivier Sobrie <olivier@sobrie.be>
2014-04-10 08:55:45 +0200

    display-confusion-table: add support for xmcda file with only learned model
    WHen using a real dataset, there is no initial and learned model.
    Only initial assignment are present. So add support for those files.

--- apps/display-confusion-table.py
+++ apps/display-confusion-table.py
@@ -7,6 +7,7 @@ from xml.etree import ElementTree
 
 from pymcda.electre_tri import MRSort
 from pymcda.types import PerformanceTable
+from pymcda.types import AlternativesAssignments
 from pymcda.utils import compute_ca
 from pymcda.utils import compute_confusion_matrix
 from pymcda.utils import print_confusion_matrix
@@ -23,15 +24,19 @@ if is_bz2_file(f) is True:
 tree = ElementTree.parse(f)
 root = tree.getroot()
 
-m1 = MRSort().from_xmcda(root, 'initial')
-m2 = MRSort().from_xmcda(root, 'learned')
-
 pt_learning = PerformanceTable().from_xmcda(root, 'learning_set')
 pt_test = PerformanceTable().from_xmcda(root, 'test_set')
 
-aa_learning_m1 = m1.pessimist(pt_learning)
+try:
+    m1 = MRSort().from_xmcda(root, 'initial')
+    aa_learning_m1 = m1.pessimist(pt_learning)
+    aa_test_m1 = m1.pessimist(pt_test)
+except:
+    aa_learning_m1 = AlternativesAssignments().from_xmcda(root, 'learning_set')
+    aa_test_m1 = AlternativesAssignments().from_xmcda(root, 'test_set')
+
+m2 = MRSort().from_xmcda(root, 'learned')
 aa_learning_m2 = m2.pessimist(pt_learning)
-aa_test_m1 = m1.pessimist(pt_test)
 aa_test_m2 = m2.pessimist(pt_test)
 
 ca_learning = compute_ca(aa_learning_m1, aa_learning_m2)
@@ -41,11 +46,11 @@ print("Learning set")
 print("============")
 print("CA: %g\n" % ca_learning)
 matrix = compute_confusion_matrix(aa_learning_m1, aa_learning_m2,
-                                  m1.categories)
+                                  m2.categories)
 print_confusion_matrix(matrix)
 
 print("\n\nTest set")
 print("========")
 print("CA: %g\n" % ca_test)
-matrix = compute_confusion_matrix(aa_test_m1, aa_test_m2, m1.categories)
+matrix = compute_confusion_matrix(aa_test_m1, aa_test_m2, m2.categories)
 print_confusion_matrix(matrix)
