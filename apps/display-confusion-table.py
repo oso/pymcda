b550168c33ca831bb0cf0c9d2757c07458a871da
Olivier Sobrie <olivier@sobrie.be>
2014-04-13 21:51:47 +0200

    apps: display-confusion-table: make it compatible with avfsort

--- apps/display-confusion-table.py
+++ apps/display-confusion-table.py
@@ -6,6 +6,7 @@ import bz2
 from xml.etree import ElementTree
 
 from pymcda.electre_tri import MRSort
+from pymcda.uta import AVFSort
 from pymcda.types import PerformanceTable
 from pymcda.types import AlternativesAssignments
 from pymcda.utils import compute_ca
@@ -27,17 +28,26 @@ root = tree.getroot()
 pt_learning = PerformanceTable().from_xmcda(root, 'learning_set')
 pt_test = PerformanceTable().from_xmcda(root, 'test_set')
 
-try:
+if root.find("ElectreTri[@id='initial']") is not None:
     m1 = MRSort().from_xmcda(root, 'initial')
     aa_learning_m1 = m1.pessimist(pt_learning)
     aa_test_m1 = m1.pessimist(pt_test)
-except:
+elif root.find("AVFSort[@id='initial']") is not None:
+    m1 = AVFSort().from_xmcda(root, 'initial')
+    aa_learning_m1 = m1.get_assignments(pt_learning)
+    aa_test_m1 = m1.get_assignments(pt_test)
+else:
     aa_learning_m1 = AlternativesAssignments().from_xmcda(root, 'learning_set')
     aa_test_m1 = AlternativesAssignments().from_xmcda(root, 'test_set')
 
-m2 = MRSort().from_xmcda(root, 'learned')
-aa_learning_m2 = m2.pessimist(pt_learning)
-aa_test_m2 = m2.pessimist(pt_test)
+if root.find("ElectreTri[@id='learned']") is not None:
+    m2 = MRSort().from_xmcda(root, 'learned')
+    aa_learning_m2 = m2.pessimist(pt_learning)
+    aa_test_m2 = m2.pessimist(pt_test)
+elif root.find("AVFSort[@id='learned']") is not None:
+    m2 = AVFSort().from_xmcda(root, 'learned')
+    aa_learning_m2 = m2.get_assignments(pt_learning)
+    aa_test_m2 = m2.get_assignments(pt_test)
 
 ca_learning = compute_ca(aa_learning_m1, aa_learning_m2)
 ca_test = compute_ca(aa_test_m1, aa_test_m2)
