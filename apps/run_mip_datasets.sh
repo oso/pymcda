e5f3c340915ce3fd00b7bdd4af22122f284d93fc
Olivier Sobrie <olivier@sobrie.be>
2014-04-14 07:53:26 +0200

    apps: run_mip_datasets: add shell script that run MRSort MIP on all datasets

---
+++ apps/run_mip_datasets.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SCRIPT=test_mip_mrsort_csv.py
+
+run_mip() {
+	dataset=$1
+	size="$2"
+	timestamp=$(date +"%Y%m%d-%H%M")
+	python $SCRIPT -i ../datasets/$dataset -p $size \
+		-f data/mip-$dataset-$timestamp.csv -s 100
+}
+
+run_mip dbs.csv 20,50,80
+run_mip cpu.csv 20,50,80
+run_mip bcc.csv 20
+run_mip mpg.csv 20
+run_mip esl.csv 20,50,80
+run_mip mmg.csv 20
+run_mip era.csv 20,50,80
+run_mip lev.csv 20,50,80
