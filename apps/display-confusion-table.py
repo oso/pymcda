add07817d149b492b9918d00b4b6a90993b02b97
Olivier Sobrie <olivier@sobrie.be>
2014-04-16 08:04:48 +0200

    apps: display-confusion-table: make it possible to read fils without test set

--- apps/display-confusion-table.py
+++ apps/display-confusion-table.py
@@ -25,42 +25,67 @@ if is_bz2_file(f) is True:
 tree = ElementTree.parse(f)
 root = tree.getroot()
 
-pt_learning = PerformanceTable().from_xmcda(root, 'learning_set')
-pt_test = PerformanceTable().from_xmcda(root, 'test_set')
+try:
+    pt_learning = PerformanceTable().from_xmcda(root, 'learning_set')
+except:
+    pt_learning = None
+
+try:
+    pt_test = PerformanceTable().from_xmcda(root, 'test_set')
+except:
+    pt_test = None
+
+aa_learning_m1, aa_learning_m2 = None, None
+aa_test_m1, aa_test_m2 = None, None
 
 if root.find("ElectreTri[@id='initial']") is not None:
     m1 = MRSort().from_xmcda(root, 'initial')
-    aa_learning_m1 = m1.pessimist(pt_learning)
-    aa_test_m1 = m1.pessimist(pt_test)
+    if pt_learning is not None:
+        aa_learning_m1 = m1.pessimist(pt_learning)
+    if pt_test is not None:
+        aa_test_m1 = m1.pessimist(pt_test)
 elif root.find("AVFSort[@id='initial']") is not None:
     m1 = AVFSort().from_xmcda(root, 'initial')
-    aa_learning_m1 = m1.get_assignments(pt_learning)
-    aa_test_m1 = m1.get_assignments(pt_test)
+    if pt_learning is not None:
+        aa_learning_m1 = m1.get_assignments(pt_learning)
+    if pt_test is not None:
+        aa_test_m1 = m1.get_assignments(pt_test)
 else:
-    aa_learning_m1 = AlternativesAssignments().from_xmcda(root, 'learning_set')
-    aa_test_m1 = AlternativesAssignments().from_xmcda(root, 'test_set')
+    if root.find("alternativesAffectations[@id='learning_set']") is not None:
+        aa_learning_m1 = AlternativesAssignments().from_xmcda(root,
+                                                              'learning_set')
+
+    if root.find("alternativesAffectations[@id='test_set']") is not None:
+        aa_test_m1 = AlternativesAssignments().from_xmcda(root, 'test_set')
 
 if root.find("ElectreTri[@id='learned']") is not None:
     m2 = MRSort().from_xmcda(root, 'learned')
-    aa_learning_m2 = m2.pessimist(pt_learning)
-    aa_test_m2 = m2.pessimist(pt_test)
+    if pt_learning is not None:
+        aa_learning_m2 = m2.pessimist(pt_learning)
+    if pt_test is not None:
+        aa_test_m2 = m2.pessimist(pt_test)
 elif root.find("AVFSort[@id='learned']") is not None:
     m2 = AVFSort().from_xmcda(root, 'learned')
-    aa_learning_m2 = m2.get_assignments(pt_learning)
-    aa_test_m2 = m2.get_assignments(pt_test)
+    if pt_learning is not None:
+        aa_learning_m2 = m2.get_assignments(pt_learning)
+    if pt_test is not None:
+        aa_test_m2 = m2.get_assignments(pt_test)
 
-ca_learning = compute_ca(aa_learning_m1, aa_learning_m2)
-ca_test = compute_ca(aa_test_m1, aa_test_m2)
+if aa_learning_m1 is not None:
+    ca_learning = compute_ca(aa_learning_m1, aa_learning_m2)
 
-print("Learning set")
-print("============")
-print("CA: %g\n" % ca_learning)
-matrix = compute_confusion_matrix(aa_learning_m1, aa_learning_m2,
-                                  m2.categories)
-print_confusion_matrix(matrix)
+    print("Learning set")
+    print("============")
+    print("CA: %g\n" % ca_learning)
+    matrix = compute_confusion_matrix(aa_learning_m1, aa_learning_m2,
+                                      m2.categories)
+    print_confusion_matrix(matrix)
 
-print("\n\nTest set")
-print("========")
-print("CA: %g\n" % ca_test)
-matrix = compute_confusion_matrix(aa_test_m1, aa_test_m2, m2.categories)
-print_confusion_matrix(matrix)
+if aa_test_m1 is not None:
+    ca_test = compute_ca(aa_test_m1, aa_test_m2)
+
+    print("\n\nTest set")
+    print("========")
+    print("CA: %g\n" % ca_test)
+    matrix = compute_confusion_matrix(aa_test_m1, aa_test_m2, m2.categories)
+    print_confusion_matrix(matrix)
