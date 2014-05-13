a73aa95e3f2a8dd6db00acf4277c6acd34446b94
Olivier Sobrie <olivier@sobrie.be>
2014-05-13 22:14:04 +0200

    learn-from-datasets: add meta_mrsortc support

--- apps/learn-from-dataset.py
+++ apps/learn-from-dataset.py
@@ -8,6 +8,11 @@ from pymcda.pt_sorted import SortedPerformanceTable
 from pymcda.types import CriterionValue, CriteriaValues
 from pymcda.utils import compute_ca
 from pymcda.learning.meta_mrsort3 import MetaMRSortPop3
+from pymcda.learning.heur_mrsort_init_profiles import HeurMRSortInitProfiles
+from pymcda.learning.lp_mrsort_weights import LpMRSortWeights
+from pymcda.learning.heur_mrsort_profiles4 import MetaMRSortProfiles4
+from pymcda.learning.lp_mrsort_mobius import LpMRSortMobius
+from pymcda.learning.heur_mrsort_profiles_choquet import MetaMRSortProfilesChoquet
 from pymcda.learning.mip_mrsort import MipMRSort
 from pymcda.learning.lp_avfsort import LpAVFSort
 from pymcda.ui.graphic import display_electre_tri_models
@@ -20,7 +25,7 @@ from test_utils import load_mcda_input_data
 from test_utils import save_to_xmcda
 
 def usage():
-    print("%s file.csv meta_mrsort|mip_mrsort|lp_utadis" % sys.argv[0])
+    print("%s file.csv meta_mrsort|meta_mrsortc|mip_mrsort|lp_utadis" % sys.argv[0])
     sys.exit(1)
 
 if len(sys.argv) != 3:
@@ -42,6 +47,15 @@ best = data.pt.get_best(data.c)
 t1 = time.time()
 
 if algo == 'meta_mrsort':
+    heur_init_profiles = HeurMRSortInitProfiles
+    lp_weights = LpMRSortWeights
+    heur_profiles = MetaMRSortProfiles4
+elif algo == 'meta_mrsortc':
+    heur_init_profiles = HeurMRSortInitProfiles
+    lp_weights = LpMRSortMobius
+    heur_profiles = MetaMRSortProfilesChoquet
+
+if algo == 'meta_mrsort' or algo == 'meta_mrsortc':
     model_type = 'mrsort'
     cat_profiles = generate_categories_profiles(data.cats)
     model = MRSort(data.c, None, None, None, cat_profiles)
@@ -49,7 +63,10 @@ if algo == 'meta_mrsort':
 
     meta = MetaMRSortPop3(nmodels, model.criteria,
                           model.categories_profiles.to_categories(),
-                          pt_sorted, data.aa)
+                          pt_sorted, data.aa,
+                          heur_init_profiles,
+                          lp_weights,
+                          heur_profiles)
 
     for i in range(0, nloop):
         model, ca_learning = meta.optimize(nmeta)
@@ -107,7 +124,7 @@ print("Model parameters:")
 cids = model.criteria.keys()
 if model_type == 'mrsort':
     model.bpt.display(criterion_ids = cids)
-    model.cv.display(criterion_ids = cids)
+    model.cv.display()
     print("lambda: %.7s" % model.lbda)
 #    display_electre_tri_models([model], [worst], [best])
 elif model_type == 'utadis':
