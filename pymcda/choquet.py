b790fbe1b1d9c6c4d59189de2d71469f0d8bf032
Olivier Sobrie <olivier@sobrie.be>
2014-06-01 12:30:36 +0200

    choquet: add functions to pass from capacities to mobius transform, etc.

---
+++ pymcda/choquet.py
@@ -0,0 +1,67 @@
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/../")
+from itertools import combinations
+from pymcda.types import CriteriaSet
+from pymcda.types import CriterionValue, CriteriaValues
+
+def capacities_to_mobius(criteria, capacities):
+    cvs = CriteriaValues()
+    n = len(criteria)
+    for i in range(1, n + 1):
+        combis = [c for c in combinations(criteria.keys(), i)]
+        for combi in combis:
+            cid = combi[0] if (i == 1) else CriteriaSet(*combi)
+
+            m = capacities[cid].value
+            for j in range(1, i):
+                combis = [c for c in combinations(combi, j)]
+                for combi2 in combis:
+                    cidj = combi2[0] if (j == 1) else CriteriaSet(*combi2)
+                    m -= cvs[cidj].value
+
+            cv = CriterionValue(cid, m)
+            cvs.append(cv)
+
+    return cvs
+
+def mobius_to_capacities(criteria, mobius):
+    cvs = CriteriaValues()
+    n = len(criteria)
+    for i in range(1, n + 1):
+        combis = [c for c in combinations(criteria.keys(), i)]
+        for combi in combis:
+            cid = combi[0] if (i == 1) else CriteriaSet(*combi)
+            v = 0
+            for j in range(1, i + 1):
+                combis = [c for c in combinations(combi, j)]
+                for combi2 in combis:
+                    cidj = combi2[0] if (j == 1) else CriteriaSet(*combi2)
+                    if cidj in mobius:
+                        v += mobius[cidj].value
+
+            cv = CriterionValue(cid, v)
+            cvs.append(cv)
+
+    return cvs
+
+def mobius_truncate(mobius, k):
+    if k < 1:
+        raise ValueError("Invalid cut value (%d)" % k)
+
+    for m in mobius:
+        if isinstance(m.id, CriteriaSet) and len(m.id) > k:
+            mobius.remove(m.id)
+
+def capacities_are_monotone(criteria, capacities):
+    n = len(criteria)
+    for i in range(2, n + 1):
+        combis = [c for c in combinations(criteria.keys(), i)]
+        for combi in combis:
+            cid = CriteriaSet(*combi)
+            combis = [c for c in combinations(combi, i - 1)]
+            for combi2 in combis:
+                cidj = combi2[0] if (i == 2) else CriteriaSet(*combi2)
+                if capacities[cidj].value > capacities[cid].value:
+                    return False
+
+    return True
