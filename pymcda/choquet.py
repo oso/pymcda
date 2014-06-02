d4246690baae06cfb4d7f2bbfc6ec2d5ba3ab9e2
Olivier Sobrie <olivier@sobrie.be>
2014-06-02 19:55:44 +0200

    choquet: remove useless intermediate variables

--- pymcda/choquet.py
+++ pymcda/choquet.py
@@ -8,14 +8,11 @@ def capacities_to_mobius(criteria, capacities):
     cvs = CriteriaValues()
     n = len(criteria)
     for i in range(1, n + 1):
-        combis = [c for c in combinations(criteria.keys(), i)]
-        for combi in combis:
+        for combi in [c for c in combinations(criteria.keys(), i)]:
             cid = combi[0] if (i == 1) else CriteriaSet(*combi)
-
             m = capacities[cid].value
             for j in range(1, i):
-                combis = [c for c in combinations(combi, j)]
-                for combi2 in combis:
+                for combi2 in [c for c in combinations(combi, j)]:
                     cidj = combi2[0] if (j == 1) else CriteriaSet(*combi2)
                     m -= cvs[cidj].value
 
@@ -28,13 +25,11 @@ def mobius_to_capacities(criteria, mobius):
     cvs = CriteriaValues()
     n = len(criteria)
     for i in range(1, n + 1):
-        combis = [c for c in combinations(criteria.keys(), i)]
-        for combi in combis:
+        for combi in [c for c in combinations(criteria.keys(), i)]:
             cid = combi[0] if (i == 1) else CriteriaSet(*combi)
             v = 0
             for j in range(1, i + 1):
-                combis = [c for c in combinations(combi, j)]
-                for combi2 in combis:
+                for combi2 in [c for c in combinations(combi, j)]:
                     cidj = combi2[0] if (j == 1) else CriteriaSet(*combi2)
                     if cidj in mobius:
                         v += mobius[cidj].value
@@ -55,11 +50,9 @@ def mobius_truncate(mobius, k):
 def capacities_are_monotone(criteria, capacities):
     n = len(criteria)
     for i in range(2, n + 1):
-        combis = [c for c in combinations(criteria.keys(), i)]
-        for combi in combis:
+        for combi in [c for c in combinations(criteria.keys(), i)]:
             cid = CriteriaSet(*combi)
-            combis = [c for c in combinations(combi, i - 1)]
-            for combi2 in combis:
+            for combi2 in [c for c in combinations(combi, i - 1)]:
                 cidj = combi2[0] if (i == 2) else CriteriaSet(*combi2)
                 if capacities[cidj].value > capacities[cid].value:
                     return False
