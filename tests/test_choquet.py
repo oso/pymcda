90125b40c91b308aea17de2150c4e12e610141d5
Olivier Sobrie <olivier@sobrie.be>
2014-06-08 17:28:28 +0200

    test_choquet: add new tests

--- tests/test_choquet.py
+++ tests/test_choquet.py
@@ -29,6 +29,37 @@ class tests_mobius(unittest.TestCase):
 
         self.assertTrue(capacities_are_monotone(c, capacities))
 
+    def test003(self):
+        random.seed(2)
+        c = generate_criteria(10)
+        capacities = generate_random_capacities(c)
+        mobius = capacities_to_mobius(c, capacities)
+
+        self.assertAlmostEqual(mobius.sum(), 1)
+
+    def test004(self):
+        random.seed(3)
+        c = generate_criteria(10)
+        capacities = generate_random_capacities(c)
+        mobius = capacities_to_mobius(c, capacities)
+        mobius_truncate(mobius, 2)
+        mobius.normalize_sum_to_unity()
+        capacities2 = mobius_to_capacities(c, mobius)
+
+        self.assertAlmostEqual(mobius.sum(), 1)
+        self.assertTrue(capacities_are_monotone(c, capacities2))
+        self.assertAlmostEqual(capacities2.max(), 1)
+
+    def test005(self):
+        random.seed(4)
+        c = generate_criteria(10)
+        capacities = generate_random_capacities(c, 2)
+        mobius = capacities_to_mobius(c, capacities)
+
+        self.assertAlmostEqual(mobius.sum(), 1)
+        self.assertTrue(capacities_are_monotone(c, capacities))
+        self.assertAlmostEqual(capacities.max(), 1)
+
 test_classes = [tests_mobius]
 
 if __name__ == "__main__":
