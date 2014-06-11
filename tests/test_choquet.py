1690dd327960ab76b2ccc427215c506d16811038
Olivier Sobrie <olivier@sobrie.be>
2014-06-11 22:08:12 +0200

    test_choquet: test001: use a seed that works both with python 2 and 3
    The first test was failing with python3 due to a rounding error.
    With another seed, the problem does not happen.

--- tests/test_choquet.py
+++ tests/test_choquet.py
@@ -12,7 +12,7 @@ from pymcda.choquet import capacities_are_monotone
 class tests_mobius(unittest.TestCase):
 
     def test001(self):
-        random.seed(3)
+        random.seed(4)
         c = generate_criteria(3)
         capacities = generate_random_capacities(c)
         mobius = capacities_to_mobius(c, capacities)
