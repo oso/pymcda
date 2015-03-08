4a4589a1c26cbbb4953cd06963db5151ebf41bdd
Olivier Sobrie <olivier@sobrie.be>
2015-03-08 13:19:58 +0100

    lp_mrsort_post_weights: update post-treatment of the weights

--- pymcda/learning/lp_mrsort_post_weights.py
+++ pymcda/learning/lp_mrsort_post_weights.py
@@ -12,10 +12,11 @@ verbose = False
 
 class LpMRSortPostWeights(object):
 
-    def __init__(self, cvs, lbda, epsilon = 0.00001):
+    def __init__(self, cvs, lbda, wsum):
         self.cvs = cvs
         self.lbda = lbda
-        self.epsilon = epsilon
+        self.epsilon = 0.01
+        self.wsum = wsum
 
         self.solver = os.getenv('SOLVER', 'cplex')
         if self.solver == 'cplex':
@@ -32,41 +33,30 @@ class LpMRSortPostWeights(object):
     def __add_variables_cplex(self):
         self.lp.variables.add(names=["w_%s" % c.id for c in self.cvs],
                               lb=[0 for c in self.cvs],
-                              ub=[1 for c in self.cvs])
-        self.lp.variables.add(names=["wm_%s" % c.id for c in self.cvs],
-                              lb=[0 for c in self.cvs],
-                              ub=[1 for c in self.cvs])
-        self.lp.variables.add(names=["wp_%s" % c.id for c in self.cvs],
-                              lb=[0 for c in self.cvs],
-                              ub=[1 for c in self.cvs])
+                              ub=[int(self.wsum / 2) for c in self.cvs],
+                              types=[self.lp.variables.type.integer
+                                     for c in self.cvs])
+        self.lp.variables.add(names=["x_%s" % fmin for fmin in self.__fmins],
+                              lb=[0 for fmin in self.__fmins])
+        self.lp.variables.add(names=["y_%s" % gmax for gmax in self.__gmaxs],
+                              lb=[0 for gmax in self.__gmaxs])
+        self.lp.variables.add(names=["alpha"], lb=[0])
         self.lp.variables.add(names=['lambda'],
-                              lb = [0], ub = [1]) #len(self.cvs)])
+                              lb = [0], ub = [self.wsum],
+                              types=[self.lp.variables.type.integer])
 
     def __add_constraints_cplex(self):
         constraints = self.lp.linear_constraints
-
-        for cv in self.cvs:
-            # wi = wi+ - wi- + 1/n
-            constraints.add(names = ["c_w_%s" % cv.id],
-                            lin_expr =
-                                [
-                                 [["w_%s" % cv.id, "wm_%s" % cv.id,
-                                   "wp_%s" % cv.id],
-                                  [1, 1, -1]],
-                                ],
-                            senses = ["E"],
-                            rhs = [1 / len(self.cvs)],
-                           )
 
         # sum(wi) = 1
         constraints.add(names = ["wsum"],
                         lin_expr =
                             [
                              [["w_%s" % cv.id for cv in self.cvs],
-                              [1.0] * len(self.cvs)],
+                              [1] * len(self.cvs)],
                             ],
                         senses = ["E"],
-                        rhs = [1],
+                        rhs = [self.wsum],
                        )
 
         # fmins
@@ -75,11 +65,11 @@ class LpMRSortPostWeights(object):
             constraints.add(names = ["fmin_%s" % fmin],
                             lin_expr =
                                 [
-                                 [wvars + ["lambda"],
-                                  [1] * len(wvars) + [-1]],
+                                 [wvars + ["lambda"] + ["x_%s" % fmin],
+                                  [1] * len(wvars) + [-1] + [-1]],
                                 ],
                             senses = ["G"],
-                            rhs = [self.epsilon],
+                            rhs = [0],
                            )
 
         # gmaxs
@@ -88,18 +78,43 @@ class LpMRSortPostWeights(object):
             constraints.add(names = ["gmax_%s" % gmax],
                             lin_expr =
                                 [
-                                 [wvars + ["lambda"],
-                                  [1] * len(wvars) + [-1]],
+                                 [wvars + ["lambda"] + ["y_%s" % gmax],
+                                  [1] * len(wvars) + [-1] + [1]],
                                 ],
                             senses = ["L"],
                             rhs = [-self.epsilon],
                            )
 
+        # alpha
+        for fmin in self.__fmins:
+            constraints.add(names = ["alpha_%s" % fmin],
+                            lin_expr =
+                                [
+                                 [["alpha"] + ["x_%s" % fmin],
+                                  [1, -1]],
+                                ],
+                            senses = ["L"],
+                            rhs = [0],
+                           )
+
+        for gmax in self.__gmaxs:
+            constraints.add(names = ["alpha_%s" % fmin],
+                            lin_expr =
+                                [
+                                 [["alpha"] + ["y_%s" % gmax],
+                                  [1, -1]],
+                                ],
+                            senses = ["L"],
+                            rhs = [0],
+                           )
+
     def __add_objective_cplex(self):
-        self.lp.objective.set_sense(self.lp.objective.sense.minimize)
-        for cv in self.cvs:
-            self.lp.objective.set_linear("wp_%s" % cv.id, 1)
-            self.lp.objective.set_linear("wm_%s" % cv.id, 1)
+        self.lp.objective.set_sense(self.lp.objective.sense.maximize)
+        self.lp.objective.set_linear("alpha", 1)
+#        for fmin in self.__fmins:
+#            self.lp.objective.set_linear("x_%s" % fmin, 1)
+#        for gmax in self.__gmaxs:
+#            self.lp.objective.set_linear("y_%s" % gmax, 1)
 
     def solve_cplex(self):
         self.__add_variables_cplex()
@@ -109,25 +124,29 @@ class LpMRSortPostWeights(object):
         self.lp.solve()
 
         status = self.lp.solution.get_status()
-        if status != self.lp.solution.status.optimal:
+        if status != self.lp.solution.status.MIP_optimal:
             raise RuntimeError("Solver status: %s" % status)
+
+#        if status != self.lp.solution.status.MIP_optimal and \
+#           status != self.lp.solution.status.optimal_tolerance:
+#            raise RuntimeError("Solver status: %s" % status)
 
         obj = self.lp.solution.get_objective_value()
 
         cvs2 = CriteriaValues()
         for cv in self.cvs:
             cv2 = CriterionValue(cv.id)
-            cv2.value = self.lp.solution.get_values("w_%s" % cv.id)
+            cv2.value = int(self.lp.solution.get_values("w_%s" % cv.id))
             cvs2.append(cv2)
 
-        lbda2 = self.lp.solution.get_values("lambda")
+        lbda2 = int(self.lp.solution.get_values("lambda"))
 
         return obj, cvs2, lbda2
 
     def solve(self):
         self.__fmins, self.__gmaxs = \
             compute_winning_and_loosing_coalitions(self.cvs,
-                                                           self.lbda)
+                                                   self.lbda)
 #        self.__fmins = compute_minimal_winning_coalitions(self.__fmins)
 #        self.__gmaxs = compute_maximal_loosing_coalitions(self.__gmaxs)
 
@@ -141,7 +160,7 @@ if __name__ == "__main__":
 
     random.seed(1)
 
-    c = generate_criteria(14)
+    c = generate_criteria(5)
     cvs = generate_random_criteria_weights(c)
     lbda = round(random.uniform(0.5, 1), 3)
 
