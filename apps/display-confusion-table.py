d02f5dac28a7976056acb7eac1e4ece28ac0c8c1
Olivier Sobrie <olivier@sobrie.be>
2016-08-15 13:08:48 +0200

    apps: display-confusion-table: display AUC too

--- apps/display-confusion-table.py
+++ apps/display-confusion-table.py
@@ -68,24 +68,47 @@ elif root.find("AVFSort[@id='learned']") is not None:
     m2 = AVFSort().from_xmcda(root, 'learned')
     if pt_learning is not None:
         aa_learning_m2 = m2.get_assignments(pt_learning)
+        aids = []
+        from pymcda.utils import print_pt_and_assignments
+        for aid in aa_learning_m2.keys():
+            if aa_learning_m2[aid].category_id != aa_learning_m1[aid].category_id:
+                aids.append(aid)
+            else:
+                aids.append(aid)
+
+        au = m2.global_utilities(pt_learning)
+        print_pt_and_assignments(aids, None, [aa_learning_m1, aa_learning_m2], pt_learning, au)
+#        for i in range(1, len(pt_learning) + 1):
+#            aid = "a%d" % i
+#            uti = m2.global_utility(pt_learning["a%d" % i])
+#            if aa_learning_m2[aid].category_id != aa_learning_m1[aid].category_id:
+#                print("%s %g %s %s" % (aid, uti.value, aa_learning_m2[aid].category_id, aa_learning_m1[aid].category_id))
+#        print_pt_and_assignments(anok, c, [aa_learning_m1, aa_learning_m2], pt_learning)
     if pt_test is not None:
         aa_test_m2 = m2.get_assignments(pt_test)
 
+def compute_auc_histo(aa):
+    pass
+
 if aa_learning_m1 is not None:
     ca_learning = compute_ca(aa_learning_m1, aa_learning_m2)
+    auc_learning = m2.auc(aa_learning_m1, pt_learning)
 
     print("Learning set")
     print("============")
-    print("CA: %g\n" % ca_learning)
+    print("CA : %g" % ca_learning)
+    print("AUC: %g\n" % auc_learning)
     matrix = compute_confusion_matrix(aa_learning_m1, aa_learning_m2,
                                       m2.categories)
     print_confusion_matrix(matrix)
 
 if aa_test_m1 is not None and len(aa_test_m1) > 0:
     ca_test = compute_ca(aa_test_m1, aa_test_m2)
+    auc_test = m2.auc(aa_test_m1, pt_test)
 
     print("\n\nTest set")
     print("========")
-    print("CA: %g\n" % ca_test)
+    print("CA : %g" % ca_test)
+    print("AUC: %g\n" % auc_test)
     matrix = compute_confusion_matrix(aa_test_m1, aa_test_m2, m2.categories)
     print_confusion_matrix(matrix)
