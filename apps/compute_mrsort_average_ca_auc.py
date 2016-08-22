d4d9f97b8a70cba88d3d5a2b091680a0293e7fa2
Olivier Sobrie <olivier@sobrie.be>
2016-08-22 19:19:59 +0200

    compute_mrsort_average_ca_auc.py: add tool to compute CA and AUC of MR-Sort

---
+++ apps/compute_mrsort_average_ca_auc.py
@@ -0,0 +1,104 @@
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/../")
+from xml.etree import ElementTree
+import bz2
+import numpy as np
+
+from pymcda.electre_tri import MRSort
+from pymcda.types import PerformanceTable
+from pymcda.types import AlternativesAssignments
+from pymcda.utils import compute_ca
+from pymcda.utils import print_confusion_matrix
+from test_utils import is_bz2_file
+
+def compute_aa(m, pt_learning, pt_test):
+    aa
+
+table_ca_learning = []
+table_ca_test = []
+table_auc_learning = []
+table_auc_test = []
+cmatrix_learning = {}
+cmatrix_test = {}
+
+for f in sys.argv[1:]:
+    if is_bz2_file(f) is True:
+        f = bz2.BZ2File(f)
+
+    tree = ElementTree.parse(f)
+    root = tree.getroot()
+    m = MRSort().from_xmcda(root, 'learned')
+
+    pt_learning = PerformanceTable().from_xmcda(root, 'learning_set')
+    pt_test = PerformanceTable().from_xmcda(root, 'test_set')
+
+    aa_learning = AlternativesAssignments().from_xmcda(root,
+                                                       'learning_set')
+    aa_test = AlternativesAssignments().from_xmcda(root,
+                                                  'test_set')
+
+    aa_learning_m2 = m.pessimist(pt_learning)
+    aa_test_m2 = m.pessimist(pt_test)
+
+    # Compute classification accuracy
+    ca_learning = compute_ca(aa_learning, aa_learning_m2)
+    ca_test = compute_ca(aa_test, aa_test_m2)
+
+    table_ca_learning.append(ca_learning)
+    table_ca_test.append(ca_test)
+
+    # Compute area under the curve
+    auc_learning = m.auc(aa_learning, pt_learning)
+    auc_test = m.auc(aa_test, pt_test)
+
+    table_auc_learning.append(auc_learning)
+    table_auc_test.append(auc_test)
+
+    # Compute confusion matrices
+    for a in aa_learning.keys():
+        key = (aa_learning[a].category_id, aa_learning_m2[a].category_id)
+        if key in cmatrix_learning:
+            cmatrix_learning[key] += 1
+        else:
+            cmatrix_learning[key] = 1
+
+    for a in aa_test.keys():
+        key = (aa_test[a].category_id, aa_test_m2[a].category_id)
+        if key in cmatrix_test:
+            cmatrix_test[key] += 1
+        else:
+            cmatrix_test[key] = 1
+
+avg_ca_learning = sum(table_ca_learning) / float(len(table_ca_learning))
+avg_ca_test = sum(table_ca_test) / float(len(table_ca_test))
+avg_auc_learning = sum(table_auc_learning) / float(len(table_auc_learning))
+avg_auc_test = sum(table_auc_test) / float(len(table_auc_test))
+
+stdev_ca_learning = np.std(np.array(table_ca_learning), axis=0)
+stdev_ca_test = np.std(np.array(table_ca_test), axis=0)
+stdev_auc_learning = np.std(np.array(table_auc_learning), axis=0)
+stdev_auc_test = np.std(np.array(table_auc_test), axis=0)
+
+nfiles = len(sys.argv) - 1
+cmatrix_learning.update((x, y / nfiles) for x, y in cmatrix_learning.items())
+cmatrix_test.update((x, y / nfiles) for x, y in cmatrix_test.items())
+
+print("CA learning avg: %g +- %g" % (avg_ca_learning, stdev_ca_learning))
+print("CA test avg: %g +- %g" % (avg_ca_test, stdev_ca_test))
+print("AUC learning avg: %g +- %g" % (avg_auc_learning, stdev_auc_learning))
+print("AUC test avg: %g +- %g" % (avg_auc_test, stdev_auc_test))
+
+print("Confusion matrix learning set")
+print_confusion_matrix(cmatrix_learning)
+print("Confusion matrix test set")
+print_confusion_matrix(cmatrix_test)
+
+cmatrix_learning.update((x, 100 * y / sum(cmatrix_learning.values()))
+                        for x, y in cmatrix_learning.items())
+cmatrix_test.update((x, 100 * y / sum(cmatrix_test.values()))
+                    for x, y in cmatrix_test.items())
+
+print("Confusion matrix learning set")
+print_confusion_matrix(cmatrix_learning)
+print("Confusion matrix test set")
+print_confusion_matrix(cmatrix_test)
